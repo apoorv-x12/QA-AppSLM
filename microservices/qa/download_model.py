4534b56ffb8dd550feb1bf2e3ea23625fd887fc4
apoorv-x12 <fifa.ajay219@gmail.com>
2025-04-10 20:57:33 +0530

    improved dockerfile and removed model download step in dockerfile

--- microservices/qa/download_model.py
+++ microservices/qa/download_model.py
@@ -1,7 +1,10 @@
 from transformers import AutoModelForQuestionAnswering, AutoTokenizer
 
 MODEL_NAME = "deepset/roberta-base-squad2"
+CACHE_DIR = "./models"
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+# Load from or store to local directory
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, cache_dir=CACHE_DIR)
+model = AutoModelForQuestionAnswering.from_pretrained(MODEL_NAME, cache_dir=CACHE_DIR)
 
-model = AutoModelForQuestionAnswering.from_pretrained(MODEL_NAME)
+print("Model and tokenizer loaded and cached to './models'.")
