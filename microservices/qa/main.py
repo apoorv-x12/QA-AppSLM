ea6bce5b8a69bc2c519ddf6f1721d8c88aaa5fdc
apoorv-x12 <fifa.ajay219@gmail.com>
2025-04-11 08:11:07 +0530

    finally resolved GA

--- microservices/qa/main.py
+++ microservices/qa/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from model import qa
 
+
 app = FastAPI(title="Question Answering API")
 class QARequest(BaseModel):
     question: str
