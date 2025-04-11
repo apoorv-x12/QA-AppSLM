5b236380bcd3ad5df4fdf7c7657d97bb2dd45200
apoorv-x12 <fifa.ajay219@gmail.com>
2025-04-11 07:08:02 +0530

    removed custom model download script

--- microservices/qa/model.py
+++ microservices/qa/model.py
@@ -1,17 +1,19 @@
 from transformers import pipeline
 
 def qa(question: str):
+
     qa_pipeline = pipeline(
         "question-answering",
-        model="deepset/roberta-base-squad2"
+         model="deepset/roberta-base-squad2"
     )
+
     context = """
     Paris is the capital and most populous city of France, with an estimated population of 2.2 million in 2020.
     It is the center of the Île-de-France region, and the seat of government of France.
     """
+
     return qa_pipeline({
         "question": question,
         "context": context
     })
 
-print(qa("What is the capital of France?"))
